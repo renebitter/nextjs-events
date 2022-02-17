c0f16922e00a8b5d985a2b2574e88101d52c240c
Rene <renebitter@gmail.com>
2022-02-17 09:12:03 -0300

    Event detail page components, styling, page not found

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,20 +1,37 @@
 import { useRouter } from 'next/router';
 import { getEventById } from '../../data/dummy-data';
 
+import ErrorAlert from '../../components/ui/error-alert';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+
 const EventId = () => {
   const router = useRouter();
-  const event = getEventById(router.query.id);
-  console.log(event);
+  const event = getEventById(router.query.eventId);
 
-  // if (event === undefined) {
-  //   return null;
-  // } else {
+  if (!event) {
+    return (
+      <ErrorAlert>
+        <p>No event found!</p>
+      </ErrorAlert>
+    );
+  }
+
   return (
-    <div>
-      <h1>Event Detail Page</h1>
-    </div>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
   );
 };
-// };
 
 export default EventId;

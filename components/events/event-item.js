82808a7df00ea89ad9d4442ceea80ec586e2e6d9
Rene <renebitter@gmail.com>
2022-02-17 07:35:56 -0300

    Add CSS to event components

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,7 +1,8 @@
 import Link from 'next/link';
 
+import classes from './event-item.module.css';
+
 function EventItem(props) {
-  const { event } = props;
   const { title, image, date, location, id } = props;
 
   const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
@@ -13,20 +14,20 @@ function EventItem(props) {
   const exploreLink = `/events/${id}`;
 
   return (
-    <li>
-      <img src={'/' + `${image}`} alt={title} />
-      <div>
-        <div>
+    <li className={classes.item}>
+      <img src={'/' + image} alt={title} />
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{title}</h2>
-          <div>
+          <div className={classes.date}>
             <time>{humanReadableDate}</time>
           </div>
-          <div>
+          <div className={classes.address}>
             <address>{formattedAddress}</address>
           </div>
         </div>
 
-        <div>
+        <div className={classes.actions}>
           <Link href={exploreLink}>EXPLORE EVENT</Link>
         </div>
       </div>

07b625ae87e9a570c5b5bc75e705624ee89a0722
Rene <renebitter@gmail.com>
2022-02-17 10:05:29 -0300

    Add filter component; Update button component:

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,22 @@
+import { useRouter } from 'next/router';
 import { getAllEvents } from '../../data/dummy-data';
 import EventList from '../../components/events/event-list';
+import EventsSearch from '../../components/events/events-search';
 
-const EventPage = () => {
+const AllEventsPage = () => {
+  const router = useRouter();
   const events = getAllEvents();
-  return <EventList items={events} />;
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
+  return (
+    <>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </>
+  );
 };
 
-export default EventPage;
+export default AllEventsPage;

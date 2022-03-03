3acd2c401ac01bd834d627e1f60b53238d5eded9
Rene <renebitter@gmail.com>
2022-03-03 09:13:13 -0300

    FIX renaming files

--- pages/api/feedback/[feedbackId].js
+++ pages/api/feedback/[feedbackId].js
@@ -1,4 +1,4 @@
-import { buildFeedbackPath, extractFeedback } from './feedback';
+import { buildFeedbackPath, extractFeedback } from './index';
 
 function handler(req, res) {
   // if (req.method === 'POST')

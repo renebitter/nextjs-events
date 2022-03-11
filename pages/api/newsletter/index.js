15c184c384312295f04f18960302a126036da9dc
Rene <renebitter@gmail.com>
2022-03-11 09:33:57 -0300

    Connect and insertOne to mongodb

--- pages/api/newsletter/index.js
+++ pages/api/newsletter/index.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
 
@@ -7,7 +9,20 @@ function handler(req, res) {
       return;
     }
 
-    console.log(userEmail);
+    const client = await MongoClient.connect(
+      process.env.MONGODB_NEWSLETTER_URI
+    );
+
+    console.log('Connected successfully to server');
+
+    const db = client.db();
+    const insertResult = await db
+      .collection('emails')
+      .insertOne({ email: userEmail });
+    console.log('Inserted documents =>', insertResult);
+
+    client.close();
+
     res.status(201).json({ message: 'Signed up!' });
   }
 }
